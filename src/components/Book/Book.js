1758a59b4022042a957511a24375f65fefac5a71
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2022-03-07 16:48:31 -0300

    Finished dashboard development

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -7,9 +7,7 @@ function Book(props) {
 
   return (
     <div className="Book">
-      <div>
-        <img className="Book__image" src={props.book.imageUrl} alt="Book cover." width="81" height="118.54" />
-      </div>
+      <img className="Book__image" src={props.book.imageUrl} alt="Book cover." width="81" height="118.54" />
       <section className="Book__details">
         <div>
           <h2 className="App__text--bold App__text--font-size--medium Book__title">{props.book.title}</h2>
--- src/components/BookCollectionNavigation/BookCollectionNavigation.js
+++ src/components/BookCollectionNavigation/BookCollectionNavigation.js
@@ -1,34 +1,86 @@
+import React from 'react'
 import previousDisabled from '../../previousDisabled.png'
 import nextDisabled from '../../nextDisabled.png'
 import previous from '../../previous.png'
 import next from '../../next.png'
 import './BookCollectionNavigation.css'
 
-function BookColectionNavigation(props) {
-  const previousButton = props.currentPage === 1 ? previousDisabled : previous
-  const nextButton = props.currentPage === props.totalPages ? nextDisabled : next
-
-  function goToPreviousPage() {
-    if (typeof props.goToPreviousPage === 'function' && props.currentPage !== 1) props.goToPreviousPage()
+export default class BookColectionNavigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { showMobileVersion: window.matchMedia('(max-width: 42.6875rem)').matches }
   }
 
-  function goToNextPage() {
-    if (typeof props.goToNextPage === 'function' && props.currentPage !== props.totalPages) props.goToNextPage()
+  componentDidMount() {
+    this.windowResizeListener = window.addEventListener('resize', this._changeNavigationConfiguration.bind(this))
   }
 
-  return (
-    <div className="BookColectionNavigation">
-      <div>Página <span className="App__text--bold">{props.currentPage}</span> de <span className="App__text--bold">{props.totalPages}</span></div>
-      <div className="BookColectionNavigation__buttons">
-        <button onClick={goToPreviousPage.bind(this)}>
-          <img src={previousButton} alt="Previous page button."></img>
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.windowResizeListener)
+  }
+
+  render() {
+    return (this.state.showMobileVersion ? this._getMobileVersion() : this._getDesktopVersion())
+  }
+
+  _getDesktopVersion() {
+    return (
+      <div className="BookColectionNavigation">
+        <div>
+          Página
+          <span className="App__text--bold"> {this.props.currentPage}</span> de
+          <span className="App__text--bold"> {this.props.totalPages}</span>
+        </div>
+        <div className="BookColectionNavigation__buttons">
+          <button onClick={this._goToPreviousPage.bind(this)}>
+            <img src={this._previousButton()} alt="Previous page button."></img>
+          </button>
+          <button onClick={this._goToNextPage.bind(this)}>
+            <img src={this._nextButton()} alt="Next page button."></img>
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  _getMobileVersion() {
+    return (
+      <div className="BookColectionNavigation">
+        <button onClick={this._goToPreviousPage.bind(this)}>
+          <img src={this._previousButton()} alt="Previous page button."></img>
         </button>
-        <button onClick={goToNextPage.bind(this)}>
-          <img src={nextButton} alt="Next page button."></img>
+        <div>
+          Página
+          <span className="App__text--bold"> {this.props.currentPage}</span> de
+          <span className="App__text--bold"> {this.props.totalPages}</span>
+        </div>
+        <button onClick={this._goToNextPage.bind(this)}>
+          <img src={this._nextButton()} alt="Next page button."></img>
         </button>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-export default BookColectionNavigation
+  _previousButton() {
+    return this.props.currentPage === 1 ? previousDisabled : previous
+  }
+
+  _nextButton() {
+    return this.props.currentPage === this.props.totalPages ? nextDisabled : next
+  }
+
+  _changeNavigationConfiguration() {
+    if (window.matchMedia('(max-width: 42.6875rem)').matches && !this.state.showMobileVersion) this.setState({ showMobileVersion: true })
+    else if (!window.matchMedia('(max-width: 42.6875rem)').matches && this.state.showMobileVersion) this.setState({ showMobileVersion: false })
+  }
+
+  _goToPreviousPage() {
+    if (typeof this.props.goToPreviousPage === 'function' && this.props.currentPage !== 1) {
+      this.props.goToPreviousPage()
+    }
+  }
+
+  _goToNextPage() {
+    if (typeof this.props.goToNextPage === 'function' && this.props.currentPage !== this.props.totalPages) this.props.goToNextPage()
+  }
+}
