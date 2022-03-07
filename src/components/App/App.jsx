0802147bf07e42e5364aa81baf16ef6ca9222bb8
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2022-03-07 17:33:01 -0300

    Organized App.jsx file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Login from '../Login/Login.jsx'
+import Dashboard from '../Dashboard/Dashboard.jsx'
+import User from '../../schema/User.js'
+import './App.css'
+
+/**
+ * React component responsible for rendering the login page,
+ * when user is not logged in, or the dashboard page, after
+ * user login.
+ */
+function App() {
+  const [user, setUser] = useState(new User())
+  const [authenticated, setAuthenticated] = useState(localStorage.getItem('token'))
+
+  function setupUser(user) {
+    setUser(new User(user))
+    setAuthenticated(true)
+  }
+
+  function getPageToRender() {
+    return authenticated ? <Dashboard user={user} /> : <Login sendUser={setupUser.bind(this)} />
+  }
+
+  return (
+    <div className="App">{getPageToRender()}</div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App/App.js'
+import App from './components/App/App.jsx'
 import reportWebVitals from './reportWebVitals.js'
 import './index.css'
 
