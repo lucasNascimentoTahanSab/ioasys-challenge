ce6efa756e556ee55a5fa31fe7f3b1d974cc4718
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2022-03-08 19:12:59 -0300

    Returned server to its previous state

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-const path = require('path')
 const express = require('express')
 const ioasysRouter = require('./src/routes/ioasysRouter')
 
 const server = express()
 const port = process.env.PORT || 5000
-const dev = server.get('env') !== 'production'
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 server.use('/', ioasysRouter)
-
-if (!dev) {
-  server.use(express.static(path.resolve(__dirname, 'build')))
-  server.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')))
-}
 
 server.listen(port, err => {
   if (err) throw err
