0ab2379d02aed139bb3a4664a8530ea362002ea7
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2022-03-08 21:12:05 -0300

    Returned App to functional component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import Login from '../Login/Login.jsx'
 import Dashboard from '../Dashboard/Dashboard.jsx'
 import User from '../../schema/User.js'
@@ -10,33 +10,26 @@ import './App.css'
  * when user is not logged in, or the dashboard page, after
  * user login.
  */
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: new User(utils.getUser()),
-      authenticated: Boolean(utils.getToken())
-    }
-  }
+function App(props) {
+  const [user, setUser] = useState(new User(utils.getUser()))
+  const [authenticated, setAuthenticated] = useState(Boolean(utils.getToken()))
 
-  render() {
-    return (
-      <div className="App">{this._getPageToRender()}</div>
-    )
-  }
-
-  _getPageToRender() {
-    return this.state.authenticated
-      ? <Dashboard user={this.state.user} openBookModal={this.props.openBookModal} />
-      : <Login sendUser={this._setupUser.bind(this)} />
-  }
-
-  _setupUser(user) {
+  function setupUser(user) {
     utils.storeUser(user)
 
-    this.setState({
-      user: new User(user),
-      authenticated: true
-    })
+    setUser(new User(user))
+    setAuthenticated(true)
   }
+
+  function getPageToRender() {
+    return authenticated
+      ? <Dashboard user={user} openBookModal={props.openBookModal} />
+      : <Login sendUser={setupUser.bind(this)} />
+  }
+
+  return (
+    <div className="App">{getPageToRender()}</div>
+  )
 }
+
+export default App
